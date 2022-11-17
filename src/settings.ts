320abd119f40618b1184f2b39d8c95d08dcea285
Tjerk <tjerk@houthuysen.nl>
2022-11-17 10:00:45 +0100

    init

--- src/settings.ts
+++ src/settings.ts
@@ -1,9 +1,9 @@
 /**
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
-export const PLATFORM_NAME = 'JQx00';
+export const PLATFORM_NAME = 'JQx00Sensor';
 
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'jqx00';
+export const PLUGIN_NAME = 'JQx00';
